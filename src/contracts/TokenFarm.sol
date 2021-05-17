ef3589f55cd573446390a6c581594627dc1e9218
realNasirMalik <realNasirMalik@gmail.com>
2021-05-17 12:35:27 -0500

    :heavy_plus_sign: Add dependencies
    Worked on Token Farm smart contract and Migration for effective deployment.

---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+contract TokenFarm {
+     // All code goes here...
+     string public name = "Dapp Token Farm";
+     DappToken public dappToken;
+     Dai public daiToken;
+
+     constructor(DappToken _dappToken, DaiToken _daiToken) public {
+          dappToken = _dappToken;
+          daiToken = _daiToken;
+     }
+}
